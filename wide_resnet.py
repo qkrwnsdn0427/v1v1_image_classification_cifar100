4ad9d62391b3428da6ca9b23c7a8e640f101d283
GeonHow <162314983+GeonHow@users.noreply.github.com>
2024-10-20 20:07:08 +0900

    Update wide_resnet.py

--- wide_resnet.py
+++ wide_resnet.py
@@ -35,8 +35,8 @@ class wide_basic(nn.Module):
             )
 
     def forward(self, x):
-        out = self.dropout(self.conv1(F.relu(self.bn1(x))))
-        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.dropout(self.conv1(F.leaky_relu(self.bn1(x))))
+        out = self.conv2(F.leaky_relu(self.bn2(out)))
         out += self.shortcut(x)
 
         return out
@@ -75,7 +75,7 @@ class Wide_ResNet(nn.Module):
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = F.relu(self.bn1(out))
+        out = F.leaky_relu(self.bn1(out))
         out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
