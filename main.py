e23063f6fa7e036cd4df5311a6970d378445253b
GeonHow <162314983+GeonHow@users.noreply.github.com>
2024-10-23 16:24:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,9 @@ seed = args.seed
 random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 torch.use_deterministic_algorithms(True)
